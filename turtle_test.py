47b44ec6f51b2d15abc93ac850d369ae4833016e
Alexis Araujo <ala1289@gmail.com>
2020-02-06 18:09:32 -0500

    Que es la programacion

---
+++ turtle_test.py
@@ -0,0 +1,21 @@
+
+# libreria
+import turtle
+
+# para que no se cierre la ventana
+window = turtle.Screen()
+
+# instancia de tortura
+tortuga = turtle.Turtle()
+
+# instrucciones
+tortuga.forward(200)
+tortuga.left(90)
+tortuga.forward(200)
+tortuga.left(90)
+tortuga.forward(200)
+tortuga.left(90)
+tortuga.forward(200)
+
+# generando ventana
+turtle.mainloop()
