90fd15525f28c9c17935dbe0848eb73ac8f74f0c
Alexis Araujo <ala1289@gmail.com>
2020-03-14 13:18:42 -0600

    Lista y eliminar contactos

--- gcloudPython/main.py
+++ gcloudPython/main.py
@@ -13,43 +13,51 @@
 # limitations under the License.
 
 # [START gae_python37_app]
-from flask import Flask, render_template, request
-# from contact_model import Contact
-
-from google.cloud import ndb
-
-
-class Contact(ndb.Model):
-    name = ndb.StringProperty()
-    phone = ndb.StringProperty()
-    email = ndb.StringProperty()
-
-
-client = ndb.Client(project='labpython')
-
+from flask import Flask, render_template, request, flash, redirect
+from contact_model import Contact
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
+app.secret_key = 'some_secret'
+app.debug = True
 
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-    """Return a friendly HTTP greeting."""
+    # contacts = Contact.query().fetch()
+    # return render_template('contact_book.html', contacts=contacts)
     return render_template('contact_book.html')
 
 
 @app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
-    with client.context():
-        if request.form:
-            contact = Contact(name=request.form.get('name'),
-                              phone=request.form.get('phone'),
-                              email=request.form.get('email'))
+    if request.form:
+        contact = Contact(name=request.form.get('name'),
+                          phone=request.form.get('phone'),
+                          email=request.form.get('email'))
 
-            contact.put()
+        contact.put()
+        flash('¡Se añadió el contacto!')
 
     return render_template('add_contact.html')
+
+
+@app.route(r'/contacts/<uid>', methods=['GET'])
+def contact_details(uid):
+    contact = Contact.get_by_id(int(uid))
+
+    if not contact:
+        return redirect('/', code=301)
+
+    return render_template('contact.html', contact=contact)
+
+
+@app.route(r'/delete', methods=['POST'])
+def delete_contact():
+    contact = Contact.get_by_id(int(request.form.get('uid')))
+    contact.key.delete()
+    return redirect('/contacts/{}'.format(contact.key.id()))
 
 
 if __name__ == '__main__':
